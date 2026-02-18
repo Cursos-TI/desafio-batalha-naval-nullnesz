88003a041d7d2382602bd92269d23bc7a113aebb
nullnesz <thsnullnes@gmail.com>
2026-02-18 01:16:23 +0000

    habilidades

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,89 +1,86 @@
 #include <stdio.h>
 
 int main() {
-     
+
     int tabuleiro[10][10];
     int TAM = 10;
     int TAM_NAVIO = 3;
 
-    for (int i = 0; i < TAM; i++) {
-        for (int j = 0; j < TAM; j++) {
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
             tabuleiro[i][j] = 0;
+
+    int linhaH = 0, colunaH = 1;
+    int linhaV = 7, colunaV = 0;
+
+    for (int i = 0; i < TAM_NAVIO; i++)
+        tabuleiro[linhaH][colunaH + i] = 3;
+
+    for (int i = 0; i < TAM_NAVIO; i++)
+        tabuleiro[linhaV + i][colunaV] = 3;
+
+    int cruz[5][5] = {
+        {0,0,3,0,0},
+        {0,0,3,0,0},
+        {3,3,3,3,3},
+        {0,0,3,0,0},
+        {0,0,3,0,0}
+    };
+
+    int octaedro[5][5] = {
+        {0,0,0,0,0},
+        {0,0,3,0,0},
+        {0,3,3,3,0},
+        {0,0,3,0,0},
+        {0,0,0,0,0}
+    };
+
+    int cone[5][5] = {
+        {0,0,0,0,0},
+        {0,0,3,0,0},
+        {0,3,3,3,0},
+        {3,3,3,3,3},
+        {0,0,0,0,0}
+    };
+
+    int centros[3][2] = {
+        {2,7},
+        {5,5},
+        {8,6}
+    };
+
+    int (*habilidades[3])[5] = {cruz, octaedro, cone};
+    int marcadores[3] = {5, 6, 7};
+
+    for (int h = 0; h < 3; h++) {
+
+        int centroLinha = centros[h][0];
+        int centroColuna = centros[h][1];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+
+                if (habilidades[h][i][j] == 3) {
+
+                    int linhaTab = centroLinha - 2 + i;
+                    int colunaTab = centroColuna - 2 + j;
+
+                    if (linhaTab >= 0 && linhaTab < TAM &&
+                        colunaTab >= 0 && colunaTab < TAM) {
+
+                        if (tabuleiro[linhaTab][colunaTab] == 0)
+                            tabuleiro[linhaTab][colunaTab] = marcadores[h];
+                    }
+                }
+            }
         }
     }
 
-    int linhaH = 2, colunaH = 3;
-    int linhaV = 6, colunaV = 1;
-
-    int linhaD1 = 0, colunaD1 = 0;   // diagonal principal
-    int linhaD2 = 0, colunaD2 = 9;   // diagonal secundária
-
-    // Horizontal
-    if (colunaH + TAM_NAVIO <= TAM) {
-        int sobreposicao = 0;
-
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            if (tabuleiro[linhaH][colunaH + i] != 0)
-                sobreposicao = 1;
-        }
-
-        if (!sobreposicao) {
-            for (int i = 0; i < TAM_NAVIO; i++)
-                tabuleiro[linhaH][colunaH + i] = 3;
-        }
-    }
-
-    // Vertical
-    if (linhaV + TAM_NAVIO <= TAM) {
-        int sobreposicao = 0;
-
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            if (tabuleiro[linhaV + i][colunaV] != 0)
-                sobreposicao = 1;
-        }
-
-        if (!sobreposicao) {
-            for (int i = 0; i < TAM_NAVIO; i++)
-                tabuleiro[linhaV + i][colunaV] = 3;
-        }
-    }
-
-    // Diagonal principal (linha e coluna aumentam juntos)
-    if (linhaD1 + TAM_NAVIO <= TAM && colunaD1 + TAM_NAVIO <= TAM) {
-        int sobreposicao = 0;
-
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            if (tabuleiro[linhaD1 + i][colunaD1 + i] != 0)
-                sobreposicao = 1;
-        }
-
-        if (!sobreposicao) {
-            for (int i = 0; i < TAM_NAVIO; i++)
-                tabuleiro[linhaD1 + i][colunaD1 + i] = 3;
-        }
-    }
-
-    // Diagonal secundária (linha aumenta, coluna diminui)
-    if (linhaD2 + TAM_NAVIO <= TAM && colunaD2 - (TAM_NAVIO - 1) >= 0) {
-        int sobreposicao = 0;
-
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            if (tabuleiro[linhaD2 + i][colunaD2 - i] != 0)
-                sobreposicao = 1;
-        }
-
-        if (!sobreposicao) {
-            for (int i = 0; i < TAM_NAVIO; i++)
-                tabuleiro[linhaD2 + i][colunaD2 - i] = 3;
-        }
-    }
-
-    printf("\n===== TABULEIRO COMPLETO =====\n\n");
+    printf("\n===== TABULEIRO FINAL =====\n\n");
 
     for (int i = 0; i < TAM; i++) {
-        for (int j = 0; j < TAM; j++) {
+        for (int j = 0; j < TAM; j++)
             printf("%d ", tabuleiro[i][j]);
-        }
         printf("\n");
     }
 
